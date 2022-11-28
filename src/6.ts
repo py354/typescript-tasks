49cada199f8b195a8d38846ef3f6b9b47fe0ec5f
Danis Kirasirov <dbgbbu@gmail.com>
2022-11-28 10:39:27 +0300

    Initial commit

---
+++ src/6.ts
@@ -0,0 +1,54 @@
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' },
+    { type: 'user', name: 'Wilson', age: 23, occupation: 'Ball' },
+    { type: 'admin', name: 'Agent Smith', age: 23, role: 'Anti-virus engineer' }
+];
+
+export function logPerson(person: Person) {
+    console.log(
+        ` - ${person.name}, ${person.age}, ${person.type === 'admin' ? person.role : person.occupation}`
+    );
+}
+
+type Criteria<T> = Omit<Partial<T>, 'type'>
+
+export function filterPersons<T extends Person>(persons: Person[], personType: 'user' | 'admin', criteria: Criteria<T>): T[] {
+    return persons
+        .filter((person): person is T => person.type === personType)
+        .filter((person) => {
+            let criteriaKeys = Object.keys(criteria) as (keyof Criteria<T>)[];
+            return criteriaKeys.every((fieldName) => {
+                return person[fieldName] === criteria[fieldName];
+            });
+        });
+}
+
+export const usersOfAge23 = filterPersons<User>(persons, 'user', { age: 23 });
+export const adminsOfAge23 = filterPersons<Admin>(persons, 'admin', { age: 23 });
+
+console.log('Users of age 23:');
+usersOfAge23.forEach(logPerson);
+
+console.log();
+
+console.log('Admins of age 23:');
+adminsOfAge23.forEach(logPerson);
